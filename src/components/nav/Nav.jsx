bdcdb132306c1b21845b0a03136c6df332d42bba
Houssem ZEMNI <houssem_zemni@outlook.com>
2022-12-29 23:07:39 +0100

    DEV floating navbar completed

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './nav.css'
+import { AiOutlineHome, AiOutlineUser } from 'react-icons/ai'
+import { BiBook } from 'react-icons/bi'
+import { RiServiceLine } from 'react-icons/ri'
+import { BiMessageSquareDots } from 'react-icons/bi'
+import { useState } from 'react'
+
+
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState("#")
   return (
-    <div>nav</div>
+    <nav>
+      <a href="#header" onClick={() => setActiveNav('#header')} className={activeNav === "#header" ? 'active' : ''}> <AiOutlineHome /> </a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === "#about" ? 'active' : ''}> <AiOutlineUser /> </a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === "#experience" ? 'active' : ''}> <BiBook /> </a>
+      <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === "#services" ? 'active' : ''}> <RiServiceLine /> </a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === "#contact" ? 'active' : ''}> <BiMessageSquareDots /> </a>
+    </nav>
   )
 }
 
