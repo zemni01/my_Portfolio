563ed0d59382e9bba5f8a2aafe87acee90434008
houssem.zemni <houssem.zemni@enim.u-monastir.tn>
2022-12-29 17:12:55 +0100

    ADD services & portfolio | FIX background image

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Experience from './components/experience/Experience'
 import Testimonials from './components/testimonials/Testimonials'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
+import Portfolio from './components/portfolio/Portfolio'
+import Services from './components/services/Services'
 
 const App = () => {
   return (
@@ -14,6 +16,8 @@ const App = () => {
         <Nav />
         <About />
         <Experience />
+        <Services />
+        <Portfolio />
         <Testimonials />
         <Contact />
         <Footer />
