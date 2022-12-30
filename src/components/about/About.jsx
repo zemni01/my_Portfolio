c84cf48ac4d8d08872875465c9d6b0aef7e580ad
Houssem ZEMNI <houssem_zemni@outlook.com>
2022-12-30 12:34:32 +0100

    DEV about me section => DONE

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/Sans titre.png'
+import { FaAward } from 'react-icons/fa'
+import { FiUsers } from 'react-icons/fi'
+import { VscFolderLibrary } from 'react-icons/vsc'
 
 const About = () => {
   return (
-    <section id='about'>about</section>
+    <section id='about' className='about_section'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className='container about_container'>
+        <div className='about_me'>
+          <div className='about_me-image'>
+            <img src={ME} alt="About me" />
+          </div>
+        </div>
+
+        <div className='about_content'>
+          <div className='about_cards'>
+            <article className='about_card'>
+              <FaAward className='about_icon' />
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+            <article className='about_card'>
+              <FiUsers className='about_icon' />
+              <h5>Clients</h5>
+              <small>5+ Clients Worldwide</small>
+            </article>
+            <article className='about_card'>
+              <VscFolderLibrary className='about_icon' />
+              <h5>Projects</h5>
+              <small>10+ Completed Projects</small>
+            </article>
+          </div>
+          <p>
+            Software development engineer freshly graduated from the national engineering school of Monastir,
+            in the field of electrical and software engineering. Details-oriented with a get-it-done, on-time,
+            and high-quality product thinking.
+          </p>
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -7,7 +7,7 @@ const HeaderSocials = () => {
   return (
     <div className='header_socials'>
         <a href="https://www.linkedin.com/in/houssem-zemni/" target="_blank" rel="noreferrer"><BsLinkedin /></a>
-        <a href="https://github.com" target="_blank" rel="noreferrer"><FaGithub /></a>
+        <a href="https://github.com/zemni01" target="_blank" rel="noreferrer"><FaGithub /></a>
         <a href="https://whatsapp.com" target="_blank" rel="noreferrer"><SiWhatsapp /></a>
     </div>
   )
