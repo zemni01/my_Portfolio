7620ef0e0da4b9ca8949a267360b40c1d8afde4f
Houssem ZEMNI <houssem_zemni@outlook.com>
2023-01-01 16:07:14 +0100

    DEV footer section => DONE | FIX nav color

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import './footer.css'
+import {FaFacebookF, FaLinkedin} from 'react-icons/fa'
+import {FiInstagram} from 'react-icons/fi'
 
 const Footer = () => {
   return (
-    <div>footer</div>
+    <footer>
+      <a href="#" className='footer_logo'>Houssem ZEMNI</a>
+
+      <ul className='permalinks'>
+        <li><a href="#header">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className='footer_socials'>
+          <a href="https://www.facebook.com/houssem.zemni/"><FaFacebookF /></a>
+          <a href="https://www.instagram.com/houssem_zemni/"><FiInstagram /></a>
+          <a href="https://www.linkedin.com/in/houssem-zemni/"><FaLinkedin /></a>
+      </div>
+
+      <div className='footer_copyright'>
+        <small>&copy; Houssem ZEMNI. All rights reserved.</small>
+      </div>
+    </footer>    
   )
 }
 
